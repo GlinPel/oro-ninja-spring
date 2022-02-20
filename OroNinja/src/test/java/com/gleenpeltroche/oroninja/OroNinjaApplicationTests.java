c5a045d7c3635819160addb264e78542721c0b60
Gleen Peltroche <gleenpeltroche@gmail.com>
2022-02-19 22:28:14 -0500

    Practica - Oro Ninja

---
+++ OroNinja/src/test/java/com/gleenpeltroche/oroninja/OroNinjaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gleenpeltroche.oroninja;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OroNinjaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
